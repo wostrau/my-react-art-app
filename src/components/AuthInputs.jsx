3c7b692cd8abc7839f9eda09a97fabe89e212981
wostrau <a.ostrovtsov@gmail.com>
2023-12-25 23:54:51 +0100

    added tailwindcss

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -4,12 +4,12 @@ import { styled } from 'styled-components';
 import Button from './Button';
 import Input from './Input';
 
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
+// const ControlContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -32,8 +32,11 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id='auth-inputs'>
-      <ControlContainer>
+    <div
+      id='auth-inputs'
+      className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'
+    >
+      <div className='flex flex-col gap-2 mb-6'>
         <Input
           type='email'
           label='Email'
@@ -46,11 +49,11 @@ export default function AuthInputs() {
           invalid={passwordNotValid}
           onChange={event => handleInputChange('password', event.target.value)}
         />
-      </ControlContainer>
-      <div className='actions'>
+      </div>
+      <div className='flex justify-end gap-4'>
         <button
           type='button'
-          className='text-button'
+          className='text-amber-400 hover:text-amber-500'
         >
           Create a new account
         </button>
